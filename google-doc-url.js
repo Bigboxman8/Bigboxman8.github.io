96707aca6b8e555879a1f990e1e3420034f0fd59
Bigboxman8 <bigboxman@hotmail.co.uk>
2024-10-25 15:26:00 +0100

    Delete google-doc-url.js

--- google-doc-url.js
+++
@@ -1,5 +0,0 @@
-// paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1y-N9aqxAwrw8cW8K8QqsJ2ZXMTRzn9is/edit?gid=1884598549#gid=1884598549';
-
-// insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
